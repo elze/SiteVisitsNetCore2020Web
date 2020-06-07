1b270c2fd2aa255654c5deef72ecf315b1f82f70
Elze Hamilton <elze.hamilton@gmail.com>
2020-06-07 15:39:41 -0500

    Added explanations to various pages of the app, added a link to Site Visits to the navigation menu, and more fields to Isp.cs entity.

--- SiteVisitsNetCore2020Web/Models/Isp.cs
+++ SiteVisitsNetCore2020Web/Models/Isp.cs
@@ -8,5 +8,8 @@ namespace SiteVisitsNetCore2020Web.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool? IsIsp { get; set; }
+        public DateTime DateCreated { get; set; }
+
     }
 }
